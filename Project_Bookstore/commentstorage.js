b05b3a9be62eda30102f5dce32958de2b3823db8
Leonhard Siegele <leonhard.siegele39@gmail.com>
2025-03-25 17:19:28 +0100

    Notizen gemacht

--- Project_Bookstore/commentstorage.js
+++ Project_Bookstore/commentstorage.js
@@ -4,20 +4,20 @@
 
 function saveComment(books) {
 
-    localStorage.setItem('books', JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));  // hier wird das ganze Array gespeichert anstatt einzelne Einträge
 };
 
 
 
 function getFromLocalStorage() {
     let booksFromStorage = JSON.parse(localStorage.getItem("books"));
-    if (booksFromStorage != null){
+    if (booksFromStorage != null){              // = nicht verwenden - dies ist ein zusweisungsoperator auch in der If-Abfrage,
         books = booksFromStorage
     }
 };
 
 function clearPool(bookIndex){
-    books[bookIndex].comments.splice(-1, books[bookIndex].comments.length);
+    books[bookIndex].comments.splice(-1, books[bookIndex].comments.length);     // durch das -1 wird ganz am Ende der letzte Eintrag gelöscht
     renderComments(bookIndex);
     
 }
